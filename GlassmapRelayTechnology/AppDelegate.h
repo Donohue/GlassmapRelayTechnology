194c20475a85edc43602d3c34b5adf9792e7b79b
Brian Donohue <brian.donohue89@gmail.com>
2013-05-13 22:34:41 -0400

    Initial Commit

---
+++ GlassmapRelayTechnology/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GlassmapRelayTechnology
+//
+//  Created by Brian Donohue on 5/13/13.
+//  Copyright (c) 2013 Brian Donohue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GlassmapRelayTechnology/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GlassmapRelayTechnology
+//
+//  Created by Brian Donohue on 5/13/13.
+//  Copyright (c) 2013 Brian Donohue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
